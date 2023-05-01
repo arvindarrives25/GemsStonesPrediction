4556692ebbfd62168c28be3c8365ce57d8cddbdb
Arvind <arvindarrives25@gmail.com>
2023-05-01 23:17:38 +0530

    Fixed Date Import error

--- src/exception.py
+++ src/exception.py
@@ -1,5 +1,5 @@
 import sys
-from src import logging
+from src.logger import logging
 
 def error_message_detail(error,error_detail:sys):
     _,_,exc_tb = error_detail.exc_info()
@@ -28,5 +28,5 @@ if __name__=="__main__":
     try:
         a=1/0
     except Exception as e:
-        logging.info('Dicision by zero') 
+        logging.info('Division by zero') 
         raise CustomException(e,sys)
